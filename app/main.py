07bfa8b1385771087a29b7b6ea3cc22ccbbb8047
Bezverkhniy Andrey <banbezv@mail.ru>
2018-10-22 01:08:25 +0300

    Non 2xx responces fixed and json-ed time sended

--- app/main.py
+++ app/main.py
@@ -1,20 +1,19 @@
+import json
+import time
+from wsgiref.util import request_uri
+
 def wsgi_application(environ, start_response):
     # бизнес-логика
     status = '200 OK'
     headers = [
         ('Content-Type', 'text/html')
     ]
-    html_code = """
-    <html>
-    <head>
-    <meta charset="utf-8">
-    <title>Boba Feta</title>
-    </head>
-    <body>
-    <h1>Hello, Gunicorn!</h1>
-    </body>
-    </html>
-    """
+    data = {
+    	"time": time.ctime(),
+    	"url": request_uri(environ)
+	}
+    html_code = json.dumps(data)
     body = html_code.encode('utf-8')
     start_response(status, headers)
     return [ body ]
+
